2f66afecc3969585c69f3bb141102d28cfca15cb
anngedeus <76603380+anngedeus@users.noreply.github.com>
2023-01-28 13:27:44 -0500

    main page layout

---
+++ theunknown/src/components/Main.js
@@ -0,0 +1,37 @@
+import {
+    makeStyles,
+    Typography,
+    Grid,
+    Card,
+    CardMedia,
+    Paper,
+} from "@mui/material"
+import React, {useState, useEffect} from 'react'
+
+export default function Main() {
+
+    return (
+        <Grid container spacing={2}>
+           <Grid item xs={12} md={8}>
+                <Paper sx={{backgroundColor: "grey"}}>
+                    <h3>User Information in this grid with profile picture</h3>
+                </Paper>
+           </Grid>
+           <Grid item xs={12} md={4}>
+                <Paper sx={{backgroundColor: "yellow"}}>
+                    <h3>other information here</h3>
+                </Paper>
+            </Grid>
+           <Grid item xs={12} md={8}>
+                <Paper sx={{backgroundColor: "pink"}}>
+                    <h3>Map here</h3>
+                </Paper>
+           </Grid>
+           <Grid item xs={12} md={4}>
+                <Paper sx={{backgroundColor: "green"}}>
+                    <h3>other information here</h3>
+                </Paper>
+           </Grid>
+        </Grid>
+    )
+}
